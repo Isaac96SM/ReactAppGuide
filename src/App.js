8a1acca562d852272b3c12be21f80b6c9b9b0f02
Isaac <suarezmota@gmail.com>
2018-03-23 11:54:34 +0100

    Using state on component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Isaac', age: Math.floor(Math.random() * 30) }, 
+      { name: 'David', age: Math.floor(Math.random() * 30) },
+      { name: 'Diana', age: Math.floor(Math.random() * 30) }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
