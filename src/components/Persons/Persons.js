2caca45f4d931ac9b5d05a7d410c8fbf44ed71c0
Isaac <Isaac96SM@github.com>
2018-03-27 16:58:32 +0200

    Using hoc's

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => {
-    return props.persons.map((person, index) => {
-        return (
-            <Person
-                key={person.id} 
-                name={person.name} 
-                age={person.age} 
-                click={() => props.clicked(index)} 
-                changed={(event) => props.changed(event, person.id)} 
-            />
-        );
-    });
+class Persons extends PureComponent {
+    constructor(props) {
+        super(props);
+        
+        console.log('[Persons.js] Inside Constructor', props);
+    }
+    
+    componentWillMount() {
+        console.log('[Persons.js] Inside componentWillMount()');
+    }
+    
+    componentDidMount() {
+        console.log('[Persons.js] Inside componentDidMount()');
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('[UPDATE Persons.js] Inside componentWillReceiveProps()', nextProps);
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside componentWillUpdate()', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('[UPDATE Persons.js] Inside componentDidUpdate()');
+    }
+
+    render () {
+        console.log('[Persons.js] Inside render()');
+
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person
+                    key={person.id} 
+                    name={person.name} 
+                    age={person.age} 
+                    click={() => this.props.clicked(index)} 
+                    changed={(event) => this.props.changed(event, person.id)} 
+                />
+            );
+        });
+    }
 }
 
-export default persons;
+export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
-class App extends Component {
-  state = {
-    persons: [
-      { id: 'fdsfscvx', name: 'Isaac', age: 21 }, 
-      { id: 'dfsfgfvx', name: 'David', age: 28 },
-      { id: 'ghfrfhgf', name: 'Diana', age: 15 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    
+    console.log('[App.js] Inside Constructor', props);
+    
+    this.state = {
+      persons: [
+        { id: 'fdsfscvx', name: 'Isaac', age: 21 }, 
+        { id: 'dfsfgfvx', name: 'David', age: 28 },
+        { id: 'ghfrfhgf', name: 'Diana', age: 15 }
+      ],
+      otherState: 'some other value',
+      showPersons: false,
+      toggleClicked: 0
+    }
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount()');
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate()');
   }
 
   deletePersonHandler = (personIndex) => {
@@ -45,10 +70,16 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+    });
   }
 
   render() {
+    console.log('[App.js] Inside render()');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -62,7 +93,8 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <Aux>
+      <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons} 
@@ -70,10 +102,10 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         />
         {persons}
-      </div>
+      </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
